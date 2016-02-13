44d6dd44663db1c5dbd03ae7a77658fab8a20c8f
Pete Travis <immanetize@fedoraproject.org>
2016-02-12 20:45:22 -0600

    set up taskwarrior environment too

--- context/source-for-environment.sh
+++ context/source-for-environment.sh
@@ -7,4 +7,7 @@
 # ANSIBLE_HOSTS is used for grouping hosts into, well, groups.
 export ANSIBLE_HOSTS=~/ansible.randomuser.org/inventory/ansible_hosts
 
+# This points to the ansible configuration file, we'll be busy in there.
 export ANSIBLE_CONFIG=~/ansible.randomuser.org/context/ansible.cfg
+
+export TASKRC=~/ansible.randomuser.org/context/taskwarrior/taskrc
