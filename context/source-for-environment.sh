68e9f5c538c01b4742036855b368fb48cf1debbe
Pete Travis <immanetize@fedoraproject.org>
2016-12-13 21:13:01 -0600

    impersonator go

--- context/source-for-environment.sh
+++ context/source-for-environment.sh
@@ -10,6 +10,8 @@ export ANSIBLE_HOSTS=~/ansible.randomuser.org/inventory/ansible_hosts
 # This points to the ansible configuration file, we'll be busy in there.
 export ANSIBLE_CONFIG=~/ansible.randomuser.org/context/ansible.cfg
 
+# out of tree modules are in a special path
+export ANSIBLE_LIBRARY=~/ansible.randomuser.org/ansible/library
 export TASKRC=~/ansible.randomuser.org/context/taskwarrior/taskrc
 
 alias check="if [[ /tmp/master-playbook.retry ]]; then RETRY_ARG='--limit @/tmp/master-playbook.retry'; else RETRY_ARG=""; fi ; ansible-playbook -C ~/ansible.randomuser.org/master-playbook.yml -vvv $RETRY_ARG"
