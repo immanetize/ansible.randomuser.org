a96bf888a6c9ffa151d7e9473bca6f1f48978cd1
Pete Travis <immanetize@fedoraproject.org>
2017-09-28 11:55:44 -0500

    dhcpd

--- context/source-for-environment.sh
+++ context/source-for-environment.sh
@@ -4,8 +4,6 @@
 # This is the first point where reusing these playbooks should differ
 # for your own environment.
 
-# ANSIBLE_HOSTS is used for grouping hosts into, well, groups.
-export ANSIBLE_HOSTS=~/ansible.randomuser.org/inventory/ansible_hosts
 
 # This points to the ansible configuration file, we'll be busy in there.
 export ANSIBLE_CONFIG=~/ansible.randomuser.org/context/ansible.cfg
